e8e3a524ced2949e6a0c3988b5cf750a7900b415
cecstudent <110324272+cecstudent@users.noreply.github.com>
2022-07-31 14:52:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
 
 app.use(express.json());
 
 
 const axios = require('axios');
-
 app.post("/", async(req,res) => {
   try {
   var body = req.body;
@@ -19,5 +17,4 @@ app.post("/", async(req,res) => {
 
 app.get('/', (req,res) => res.send('OK'))
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+app.listen(3000);
